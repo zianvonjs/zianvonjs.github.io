cdf64691e4b62f3a1852c39417061bd87ef20674
zian-mao <maozian991525@gmail.com>
2025-07-06 14:25:22 +0800

    Update lang.js

--- static/js/lang.js
+++ static/js/lang.js
@@ -22,12 +22,20 @@ const lang = {
 };
 
 function setLang(language) {
-  document.querySelector("a[href='#home']").innerText = lang[language].home;
-  document.querySelector("a[href='#job-intention']").innerText = lang[language].job_intention ;
-  document.querySelector("a[href='#awards']").innerText = lang[language].awards;
-  document.querySelector("a[href='#experience']").innerText = lang[language].experience;
-  document.querySelector("a[href='#techstack']").innerText = lang[language].techstack;
-  document.querySelector("a[href='#publications']").innerText = lang[language].publications;
-  document.getElementById("github-link").innerText = lang[language].github;
-  document.getElementById("license-link").innerText = lang[language].license;
+  const navMap = [
+    ["a[href='#home']", "home"],
+    ["a[href='#job-intention']", "job_intention"],
+    ["a[href='#awards']", "awards"],
+    ["a[href='#experience']", "experience"],
+    ["a[href='#techstack']", "techstack"],
+    ["a[href='#publications']", "publications"]
+  ];
+  navMap.forEach(([selector, key]) => {
+    const el = document.querySelector(selector);
+    if (el) el.innerText = lang[language][key];
+  });
+  const github = document.getElementById("github-link");
+  if (github) github.innerText = lang[language].github;
+  const license = document.getElementById("license-link");
+  if (license) license.innerText = lang[language].license;
 }
