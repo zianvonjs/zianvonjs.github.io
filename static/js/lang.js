b2ce9f8cd8db66ebebd6405d4d1cb4d72176129c
zian-mao <maozian991525@gmail.com>
2025-07-06 13:55:00 +0800

    1
    1

--- static/js/lang.js
+++ static/js/lang.js
@@ -9,9 +9,10 @@ const lang = {
   },
   zh: {
     home: "首页",
-    awards: "奖项",
-    experience: "经历",
-    publications: "发表作品",
+    awards: "奖惩情况",
+    experience: "工作经历",
+    techstack: "技术栈",
+    publications: "发表论文",
     github: "代码仓库",
     license: "许可协议"
   }
@@ -21,6 +22,7 @@ function setLang(language) {
   document.querySelector("a[href='#home']").innerText = lang[language].home;
   document.querySelector("a[href='#awards']").innerText = lang[language].awards;
   document.querySelector("a[href='#experience']").innerText = lang[language].experience;
+  document.querySelector("a[href='#techstack']").innerText = lang[language].techstack;
   document.querySelector("a[href='#publications']").innerText = lang[language].publications;
   document.getElementById("github-link").innerText = lang[language].github;
   document.getElementById("license-link").innerText = lang[language].license;
